kevinoid/promise-nodeify

commit 95b08f24dc08dbe4fa58525d38d70a0784e4b130

Style fixes for new ESLint rules
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

benchmark/index.js
@@ -9,9 +9,6 @@
 // For convenience, it's easier to require modules inline in this file.
 /* eslint-disable global-require */
 
-// The modules tested in this file are installed from benchmark/package.json
-/* eslint-disable node/no-missing-require */
-
 // Save native promise (if any) early since:
 // es-native will overwrite native promise when required
 // native-promise-only will overwrite if falsey when required
@@ -265,8 +262,6 @@ function formatResultsTxt(suite, useColor) {
 
 function runSuite(suite, options, cb) {
   let currentBenchmark;
-  // FIXME: https://github.com/sindresorhus/eslint-plugin-unicorn/issues/374
-  /* eslint-disable unicorn/consistent-function-scoping */
   function onBenchmarkError(err) {
     // Set the error property of the benchmark, as the internals would do
     currentBenchmark.error = err;
@@ -287,9 +282,9 @@ function runSuite(suite, options, cb) {
   }
   function benchmarkComplete() {
     clearTimeout(benchmarkTimeout);
-    benchmarkTimeout = null;
+    benchmarkTimeout = undefined;
     process.removeListener('uncaughtException', onBenchmarkError);
-    currentBenchmark = null;
+    currentBenchmark = undefined;
   }
   /* eslint-enable unicorn/consistent-function-scoping */
   suite.forEach((bench) => {
@@ -318,7 +313,7 @@ function runSuite(suite, options, cb) {
       );
     })
     .on('complete', function() {
-      done(null, this);
+      done(undefined, this);
     });
 
   defineGlobals();

index.js
@@ -31,6 +31,8 @@ function doCallback(callback, reason, value) {
     if (reason) {
       callback(reason);
     } else {
+      // Use null to preserve current API behavior.
+      // eslint-disable-next-line unicorn/no-null
       callback(null, value);
     }
   } catch (err) {
@@ -84,6 +86,8 @@ function promiseNodeify(promise, callback) {
   }
 
   function onResolved(value) {
+    // Use null to preserve current API behavior.
+    // eslint-disable-next-line unicorn/no-null
     doCallback(callback, null, value);
   }
 

test/promise-nodeify.js
@@ -91,8 +91,6 @@ describe('promiseNodeify', () => {
       const promise = Promise.resolve(value);
 
       let unhandledRejection;
-      // FIXME: https://github.com/sindresorhus/eslint-plugin-unicorn/issues/374
-      // eslint-disable-next-line unicorn/consistent-function-scoping
       function onUnhandledRejection(reason) {
         unhandledRejection = reason;
       }

commit 3fc435578301774c154e580883860f0c3e554b05

Remove unpaired eslint-enable directive
eslint-disable unicorn/consistent-function-scoping was removed in
95b08f24dc08, but the corresponding eslint-enable was not.  Fix it.

Fixes: 95b08f24dc08 ("Style fixes for new ESLint rules")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

benchmark/index.js
@@ -287,7 +287,6 @@ function runSuite(suite, options, cb) {
     process.removeListener('uncaughtException', onBenchmarkError);
     currentBenchmark = undefined;
   }
-  /* eslint-enable unicorn/consistent-function-scoping */
   for (const bench of suite) {
     bench.on('start', benchmarkStart);
     bench.on('complete', benchmarkComplete);